2069e004d34ed8c3db114a086effe5b18eba5437
MrNeutron <rspandit468@gmail.com>
2023-10-18 18:48:11 +0530

    Tried using POST method, still nothing

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ app.listen(port, function () {
 	console.log(`Backend Node Server Active on Port: ${port}`);
 });
 app.get('/', function (req, res) {
-	console.log(msg);
-	// const msg = document.getElementById('enteredMsg').value;
 	res.sendFile(__dirname + '/index.html');
 });
 app.get('/chatbot', (req, res) => {
 	res.sendFile(__dirname + '/bot.html');
 });
-app.get('/api', (req, res) => {
+app.post('/api', (req, res) => {
 	const msg = req.headers.message;
+	console.log('Loaded Server.js ' + msg);
+	res.send(msg ? msg : 'Failed');
 });
